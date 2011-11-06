60886d1c5fa36f86cc465771e693c4b185176398
Artem Leschev <wolf@gwerewolf.ru>
2011-11-07 00:10:06 +0400

    Throw expection on connecting error

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -4,6 +4,9 @@ class DatabaseConnector {
 
 	function __construct($host, $login, $password, $database) {
 		$this->mysqli = new mysqli($host, $login, $password, $database);
+		if($this->mysqli->connect_error) {
+			throw new Exception($this->mysqli->connect_error);
+		}
 	}
 }
 ?>
